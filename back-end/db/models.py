140868ba609dfe1bbbdbb9fe3daf78b332a867ac
Chris <chrisldiep@gmail.com>
2020-12-30 20:33:49 -0800

    Added schema and methods to add poe.ninja currency information to db

--- back-end/db/models.py
+++ back-end/db/models.py
@@ -1,6 +1,7 @@
 from mongoengine import *
 from datetime import datetime, timezone
 
+
 class Listing(EmbeddedDocument):
     name = StringField(required=True)
     last_char = StringField(required=True)
@@ -11,9 +12,21 @@ class Listing(EmbeddedDocument):
     has_rate = IntField()
     has_stock = IntField()
 
+
 class Currency(Document):
     currency_name = StringField(required=True)
     icon = StringField(required=True)
     last_updated = DateTimeField(default=datetime.now(timezone.utc))
     prices = MapField(ListField(EmbeddedDocumentField(Listing)))
 
+
+class ChaosEquivListing(EmbeddedDocument):
+    buy_price = DecimalField(precision=1, null=True)
+    buy_listings = IntField(null=True)
+    sell_price = DecimalField(precision=1, null=True)
+    sell_listings = IntField(null=True)
+
+
+class ChaosEquivalent(Document):
+    last_updated = DateTimeField(default=datetime.now(timezone.utc))
+    info = MapField(EmbeddedDocumentField(ChaosEquivListing))
