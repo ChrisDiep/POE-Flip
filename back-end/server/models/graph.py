f8419b7422d38aeb91ebbf19db671fb140230a38
Chris <chrisldiep@gmail.com>
2021-01-01 15:10:35 -0800

    Added docstring descriptions

--- back-end/server/models/graph.py
+++ back-end/server/models/graph.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class Graph:
+    """ Class for creating and analyzing a graph of currencies """
     def __init__(self, data):
         self.data = data
         self.currencies = self._parse_currencies(data)
@@ -10,12 +11,14 @@ class Graph:
         self.create_graph()
 
     def _parse_currencies(self, data):
+        """ Maps the currencies from the database information to an array """
         currencies = []
         for entry in data:
             currencies.append(entry["currency_name"])
         return currencies
 
     def create_graph(self):
+        """ Adds edges and nodes to the graph from information in the database """
         self.graph.add_nodes_from(self.currencies)
         for entry in self.data:
             currency_name = entry["currency_name"]
@@ -27,11 +30,16 @@ class Graph:
                 )
 
     def _add_edges(self, start, end, listings):
+        """
+        Adds edges to the nodes in the graph going from have to want
+        """
         self.graph.add_edge(start, end, trades=listings)
 
     def print(self):
+        """ Returns a string representation of the nodes """
         return " ".join(list(self.graph.nodes))
 
     def visualize(self):
+        """ Creates a static visualization of the graph """
         nx.draw(self.graph, with_labels=True, font_weight="bold")
         plt.show()
